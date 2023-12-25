f385fb4f2d0f32a089ae5f99ac8786b7bf2d23f2
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-25 16:41:33 +0300

    then catch tried

--- trial.js
+++ trial.js
@@ -1,15 +1,28 @@
 
 // ----- PROMISE -------
 
-let check =true
+// let check =true
 
-const promise1 = new Promise((resolve, reject) => {
-    if(check){
-        resolve("Promise is Succesful")
-    }else {
-        reject("Promise is Unsuccesful")
-    }
-})
+// const promise1 = new Promise((resolve, reject) => {
+//     if(check){
+//         resolve("Promise is Succesful")
+//     }else {
+//         reject("Promise is Unsuccesful")
+//     }
+// })
 
+// console.log(promise1)
 
-console.log(promise1)
+let check =false
+
+function createPromise(){
+    return new Promise((resolve, reject) => {
+        if (check) {
+            resolve("Promise running as aimed")
+        } else {reject ("There is big problem")}
+    })
+}
+createPromise()
+.then((response)=> console.log(response))
+.catch((error)  => console.log(error))
+.finally(()  => console.log("Always running"))
