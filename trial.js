222adb817a9e7b3cbb7ac085bdce2659d6438540
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-30 18:22:59 +0300

    attributes1 tried

--- trial.js
+++ trial.js
@@ -4,18 +4,11 @@
 let value;
 
 value =document.getElementById("todoAddButton");
-
-
-
-
-
 console.log("Value :",value);
-// console.log("Id  :", value.id);
-// console.log("Attribute :", value.getAttribute("id"));
-// console.log("Class Name :", value.className);
-console.log("Class Name :", value.getAttribute("class"));
 
-value = value.classList[3]
+
+value = value.textContent
 console.log("Value :",value);
+console.log("Value :",value.innerHTML);
 
 
