a445553bdfbc8b22ae4280d3bbc48bdb5e4a1a44
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-31 11:38:45 +0300

    Style properties

--- trial.js
+++ trial.js
@@ -1,16 +1,41 @@
 
-// Selectors property of style
-// class, id, tag name
-let value;
+// Style Properties
+// 
+// let value, lengthElmnt;
+// value =document.querySelectorAll("li:nth-child(odd)");
 
-value =document.getElementsByClassName("list-group-item");
-const arrayValue = Array.from(value)
+const todo = document.querySelectorAll(".list-group-item")[0].style;
+const todoList = document.querySelector(".list-group").style;
+const clearButton = document.querySelector("#todoClearButton").style;
 
-arrayValue.forEach(function(toDo1, index){
-    console.log(index+1, ". Class ",toDo1.className)
-    console.log(toDo1.textContent)
-})
-console.log("Classes :",arrayValue);
+todo.color = "#fff";
+todo.backgroundColor = "purple";
+todo.fontWeight = "bold";
+todo.paddingTop = "20px";
+todo.paddingLeft = "50px";
+
+todoList.marginTop ="60px";
+todoList.marginLeft ="60px";
+
+clearButton.backgroundColor="red";
+clearButton.fontWeight="bold";
+clearButton.padding="6px";
+clearButton.borderRadius ="5px"
+clearButton.borderColor = "brown"
+
+
+console.log("Todo :",todo);
+
+
+
+// const arrayValue = Array.from(value)
+
+// arrayValue.forEach(function(toDo1, index){
+//     toDo1.style.backgroundColor = "lightgrey"
+//     // console.log(index+1, ". Form ",toDo1)
+// })
+
+
 
 
 
